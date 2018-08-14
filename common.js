f8fb386e4c09670491887acbb03db0d468b989ef
guoze516 <2743216431@qq.com>
2018-08-14 17:51:47 +0800

    1
    Signed-off-by: guoze123 <1030124836@qq.com>

--- common.js
+++ common.js
@@ -69,6 +69,42 @@ var guoze = (function() {
     var test=function(a){
     	console.log(a)
     }
+
+    var down_more=function(){ // 
+    	//获取滚动条当前的位置 
+		function getScrollTop() {
+		    var scrollTop = 0;
+		    if (document.documentElement && document.documentElement.scrollTop) {
+		        scrollTop = document.documentElement.scrollTop;
+		    } else if (document.body) {
+		        scrollTop = document.body.scrollTop;
+		    }
+		    return scrollTop;
+		}
+
+		//获取当前可视范围的高度 
+		function getClientHeight() {
+		    var clientHeight = 0;
+
+		    if (document.body.clientHeight && document.documentElement.clientHeight) {
+		        clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight);
+
+		    } else {
+		        clientHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight);
+
+		    }
+		    return clientHeight;
+		}
+		//获取文档完整的高度 
+		function getScrollHeight() {
+		    return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+		}
+
+		if (getScrollTop() + getClientHeight() == getScrollHeight()) {
+		    more_list()
+		}
+    }
+	
     return {
     	test,remove_cache
     }
